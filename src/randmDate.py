2c3cf3ab94881a094b9f89399ced22fd7286e2e3
kfaraaz <kfaraaz@maprtech.com>
2016-09-27 10:54:50 +0000

    updated random date generator to generate many random dates

--- src/randmDate.py
+++ src/randmDate.py
@@ -1,8 +1,9 @@
-## To print a random date value.
+## To print random date values.
 
 import datetime
 import random
 from random import randint
-somedate=datetime.date(randint(1945,2015),randint(1,12),randint(1,28))
-date=somedate+datetime.timedelta(random.randint(1,365))
-print date
+for i in range(1,50):
+    somedate=datetime.date(randint(1945,2015),randint(1,12),randint(1,28))
+    date=somedate+datetime.timedelta(random.randint(1,365))
+    print date
