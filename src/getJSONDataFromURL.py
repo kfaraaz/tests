f592ab7e681c9a571243684e1ab95993047fb5f8
kfaraaz <kfaraaz@maprtech.com>
2016-10-15 17:42:41 +0000

    getJSONDataFromURL.py

--- src/getJSONDataFromURL.py
+++ src/getJSONDataFromURL.py
@@ -1,15 +1,23 @@
-import sys
+# To run this script, run it as shown below
+# python getJSONDataFromURL.py -u https://commondatastorage.googleapis.com/ckannet-storage/2012-02-07T223547/PTStateBudget2012.json
+# Do NOT forget to pass the URL as an input
+
+import argparse
 import json
 from urllib import urlopen
 
 
 def getJSONDataFromURL():
     """ Read JSON data from URL and write it to a file"""
-    url = sys.argv[1]
+    parser = argparse.ArgumentParser(description='This script takes a URL as an input')
+    parser.add_argument('-u','--ur',action='store',dest='url',default=None,help='<Required> url link',required=True)
+    results = parser.parse_args()
+    url = results.url
     u = urlopen(url)
     data = u.read()
     with open('data.json', 'w') as f:
         json.dump(json.JSONDecoder().decode(data), f)
 
 
-getJSONDataFromURL()
+if __name__ == "__main__":
+    getJSONDataFromURL()
